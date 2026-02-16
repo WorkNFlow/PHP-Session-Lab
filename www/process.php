4248a84e84038d5af333260c229dabe4c894c485
Семён <semyon.lyam@gmail.com>
2026-02-16 17:23:41 +0200

    Step 4 and 5 Done | Added data saving and displeying functionality

--- www/process.php
+++ www/process.php
@@ -7,5 +7,8 @@ $email = htmlspecialchars($_POST['film_name'] ?? '');
 $_SESSION['name'] = $username;
 $_SESSION['film_name'] = $_POST['film_name'];
 
+$line = $username . ";" . $email . "\n";
+file_put_contents("data.txt", $line, FILE_APPEND);
+
 header("Location: index.php");
 exit();
